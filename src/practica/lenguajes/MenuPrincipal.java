64ed6fcf41220c514f6ca90a7f0d694fcaaf6a77
LouisGonzalez <louiscrack50@gmail.com>
2019-07-26 08:34:55 -0600

    Envio al repositorio

---
+++ src/practica/lenguajes/MenuPrincipal.java
@@ -0,0 +1,13 @@
+package practica.lenguajes;
+/**
+ *
+ * @author luisGonzalez
+ */
+public class MenuPrincipal {
+        public static void main(String[] args) {
+            System.out.println("Hola mundo");
+
+    }
+}
+
+    
